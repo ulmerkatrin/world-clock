80121b2c29a12ae47608b199a5a9a25d174aa7b7
ulmerkatrin <ulmer.katrin4@gmail.com>
2026-05-12 21:31:50 +0200

    added js for select cities, that correct timezones and names are shown

--- src/index.js
+++ src/index.js
@@ -24,3 +24,20 @@ newYorkTimeElement.innerHTML = newYorkTime.format("h:mm:ss a");
 
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity (event) {
+    let cityTimeZone = event.target.value
+    let cityTime = moment().tz(cityTimeZone);
+    let cityName = cityTimeZone.replace("_", " ").split("/") [1];
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+    <div>
+    <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+        <div class="time">${cityTime.format("h:mm:ss a")}</div>`
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
